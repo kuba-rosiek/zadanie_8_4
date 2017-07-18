4e0db7941ecd8dba40825dea5b6997323b6ffe48
unknown <rosiek.kuba@gmail.com>
2017-07-18 16:09:42 +0100

    dodane komentarze

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// scripts.js
+
+var name = prompt('Enter your name'); //deklaracja zmienniej name
+alert('Hello, ' + name); //wyswietla okno 
+console.log('Hello, ' + name); // wyswietlanie w konsoli
